14efdf3694dc9b5ae78e28279e9b42ee159eb377
Sandesh Bohora <sandeshbohora2020@gmail.com>
2026-06-23 15:47:05 +0545

    merge

--- git.js
+++ git.js
@@ -7,4 +7,31 @@
 // * git config --global --list
 // * git confif --global user.name""
 // * git confif --global user.email"@.com"
-// * git confif --global user.""
+// * git confif --global init.defaultbranch "main"
+
+//  repositary
+//  local ->
+// remote
+
+// ? initialize hit repo
+// * git init   // initialized empty git repository
+
+// ! git work flow
+
+// * git status
+// * git add ,file_pate>
+// * git commit -m"<commit_message>"
+// * git push origin <branch_name> </branch_name>
+
+// ? remote
+// *  git remote -v
+// * git remote add origin <remote url>
+// </remote>
+
+// todo : branch
+
+// *git branch-> list all local branches
+// * git branch <branch name> create new branch</branch>
+// *git switch <branch name>
+// * git merge <branch name>
+// </branch>
